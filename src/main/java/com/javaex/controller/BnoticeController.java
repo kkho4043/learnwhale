86539a7d77a80a4b69fbc2d6e3ea30220174d7f1
teresa229 <scarlett_29@naver.com>
2021-03-17 13:00:48 +0900

    test

--- src/main/java/com/javaex/controller/BnoticeController.java
+++ src/main/java/com/javaex/controller/BnoticeController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.javaex.controller;
 
 import org.springframework.stereotype.Controller;
@@ -9,70 +8,27 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/notice")
 public class BnoticeController {
 
-	@RequestMapping(value ="/list", method = {RequestMethod.GET, RequestMethod.POST})
+	@RequestMapping(value = "/list", method = { RequestMethod.GET, RequestMethod.POST })
 	public String list() {
 		System.out.println("[BnoticeController.list()]");
 		return "ban/notice/list";
 	}
-	
-	@RequestMapping(value ="/read", method = {RequestMethod.GET, RequestMethod.POST})
+
+	@RequestMapping(value = "/read", method = { RequestMethod.GET, RequestMethod.POST })
 	public String read() {
 		System.out.println("[BnoticeController.read()]");
 		return "ban/notice/read";
 	}
-	
-	@RequestMapping(value ="/modifyForm", method = {RequestMethod.GET, RequestMethod.POST})
+
+	@RequestMapping(value = "/modifyForm", method = { RequestMethod.GET, RequestMethod.POST })
 	public String modifyForm() {
 		System.out.println("[BnoticeController.modifyForm()]");
 		return "ban/notice/modifyForm";
 	}
-	
-	@RequestMapping(value ="/modify", method = {RequestMethod.GET, RequestMethod.POST})
-	public String modify() {
-		System.out.println("[BnoticeController.modify()]");
-		return "ban/notice/list";
-	}
-	
-	
+
 	@RequestMapping(value ="/writeForm", method = {RequestMethod.GET, RequestMethod.POST})
 	public String writeForm() {
 		System.out.println("[BnoticeController.writeForm()]");
 		return "ban/notice/writeForm";
 	}
 }
-=======
-package com.javaex.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-@Controller
-@RequestMapping("/notice")
-public class BnoticeController {
-
-	@RequestMapping(value ="/list", method = {RequestMethod.GET, RequestMethod.POST})
-	public String list() {
-		System.out.println("[BnoticeController.list()]");
-		return "ban/notice/list";
-	}
-	
-	@RequestMapping(value ="/read", method = {RequestMethod.GET, RequestMethod.POST})
-	public String read() {
-		System.out.println("[BnoticeController.read()]");
-		return "ban/notice/read";
-	}
-	
-	@RequestMapping(value ="/modifyForm", method = {RequestMethod.GET, RequestMethod.POST})
-	public String modifyForm() {
-		System.out.println("[BnoticeController.modifyForm()]");
-		return "ban/notice/modifyForm";
-	}
-	
-	@RequestMapping(value ="/writeForm", method = {RequestMethod.GET, RequestMethod.POST})
-	public String writeForm() {
-		System.out.println("[BnoticeController.writeForm()]");
-		return "ban/notice/writeForm";
-	}
-}
->>>>>>> branch 'master' of https://github.com/kkho4043/learnwhale.git
