24bd122d93527b54808f9e6d96ecd18b7bebcceb
kkho4043 <kkho4043@naver.com>
2021-03-26 18:57:28 +0900

    강태욱

--- src/main/java/com/javaex/dao/BanmainDao.java
+++ src/main/java/com/javaex/dao/BanmainDao.java
@@ -7,6 +7,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.javaex.vo.ClassVo;
 import com.javaex.vo.JoinUserVo;
 
 @Repository
@@ -20,6 +21,13 @@ public class BanmainDao {
 		map.put("url",url);
 		map.put("userNo", userNo);	
 		return sqlSession.selectOne("banmain.selectjoinuser",map);
+	}
+	
+	public ClassVo selectclassInfo(String url) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("url",url);
+			
+		return sqlSession.selectOne("banmain.selectjoinclass",url);
 	}
 	
 }
--- src/main/java/com/javaex/service/BanmainService.java
+++ src/main/java/com/javaex/service/BanmainService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.javaex.dao.BanmainDao;
+import com.javaex.vo.ClassVo;
 import com.javaex.vo.JoinUserVo;
 
 @Service("/BanmainService")
@@ -22,10 +23,22 @@ public class BanmainService {
 		int userNo = (int) session.getAttribute("authUser");
 		JoinUserVo jvo = banmainDao.selectuserInfo(url, userNo);
 		
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("uname",jvo.getUsername());
-		map.put("utype", jvo.getType());
 		
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("joinVo",jvo); //접속유저 정보
+		
+		ClassVo cvo = banmainDao.selectclassInfo(url);
+		
+		map.put("ctitle","여기");
+		map.put("startdate","여기");
+		map.put("cenddate","여기");
+		
+		
+		map.put("cuname","여기");
+		map.put("cuid","여기");
+		map.put("cuemail","여기");
+		map.put("cuprofile","여기");
+		map.put("cuphoneNum","여기");
 		
 		return null;
 	}
